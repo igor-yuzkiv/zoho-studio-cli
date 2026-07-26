db5fb6c2606206a4e74446e66a43c4c3873878a7
Igor Yuzkiv <igor97@gmail.com>
2026-07-26 14:39:51 +0300

    add cli-progress

--- src/commands/debug/debug.command.ts
+++ src/commands/debug/debug.command.ts
@@ -1,5 +1,32 @@
 import { Command } from 'commander'
+import cliProgress from 'cli-progress'
+
 
 export const debugCommand = new Command('debug').description('debug').action(async () => {
     console.log('Debug')
+
+    const bar = new cliProgress.SingleBar(
+        {
+            format: 'Pulling functions |{bar}| {value}/{total} | {name}',
+            hideCursor: true,
+            clearOnComplete: false,
+        },
+        cliProgress.Presets.shades_classic
+    )
+
+
+    bar.start(10, 0, {
+        name: 'Starting...',
+    })
+
+
+    bar.update({
+        name: "test 1",
+    })
+    bar.increment()
+
+    bar.update({
+        name: 'test 1',
+    })
+    bar.increment()
 })
